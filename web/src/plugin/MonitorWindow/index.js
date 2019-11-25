e68954c122a541eddcbf9cd4b7b5370ecb310013
FlorianLebrun <florian.lebrun.pv1@gmail.com>
2019-11-25 12:32:50 +0100

    chore: monitor

---
+++ web/src/plugin/MonitorWindow/index.js
@@ -0,0 +1,42 @@
+import React from "react"
+import Application from "@application"
+import TreeNode from "@modules/TreeNode"
+import { NestedFlameChart } from "../modules/charts/chart-flame"
+
+export default class MonitorWindow extends Application.WindowComponent {
+  props: Object
+  state = {
+    profiles: []
+  }
+
+  componentWillMount() {
+    const { inspector } = this.props
+    inspector.eventSoure.addEventListener("/runtime/profile", (e) => {
+      const { profiles } = this.state
+      this.setState({ profiles: [...profiles, JSON.parse(e.data)] })
+    })
+  }
+  render() {
+    const { profiles } = this.state
+    return (<div className="flex-col" style={styles.body}>
+      {profiles && profiles.map((profile, i) => {
+        return <TreeNode
+          key={i}
+          header={`Profile Histogram: ${profile.title || i}`}
+          content={<React.Fragment>
+            <NestedFlameChart histogram={profile.histogram} style={{ height: 500 }} />
+          </React.Fragment>}
+        />
+      })}
+    </div>)
+  }
+}
+
+const styles = {
+  body: {
+    minHeight: "100%",
+    backgroundColor: "white",
+    color: "rgb(20,20,20)",
+    font: "consolas, sans-serif",
+  },
+}
--- web/src/plugin/index.js
+++ web/src/plugin/index.js
@@ -1,14 +1,17 @@
 import Application from "@application"
 
 import ProfilerWindow from "./ProfilerWindow"
+import MonitorWindow from "./MonitorWindow"
 import MemoryWindow from "./MemoryWindow"
 import StatsWindow from "./StatsWindow"
 import TerminalWindow from "./TerminalWindow"
 
 class ProfilerInspector {
   baseUrl: string
+  eventSoure: EventSource
   constructor(baseUrl) {
     this.baseUrl = baseUrl
+    this.eventSoure = new EventSource(baseUrl + "/.events")
   }
   fetchAPI(url, options) {
     return Application.fetchAPI(this.baseUrl + url, options)
@@ -21,9 +24,9 @@ class ProfilerInspector {
 class SatMemoryPlugin extends Application.PluginInstance {
   inspector: ProfilerInspector
   pluginDidMount() {
-    this.inspector = new ProfilerInspector("http://localhost:9944")
+    this.inspector = new ProfilerInspector("http://localhost:9944/admin")
     //this.openWindow("memory")
-    this.openWindow("profiler")
+    this.openWindow("monitor")
     this.openWindow("stats")
     //this.openWindow("terminal")
   }
@@ -51,6 +54,15 @@ Application.installPlugin({
       defaultIcon: "globe",
       defaultDockId: "center",
       component: ProfilerWindow,
+      parameters: {
+        "inspector": true,
+      }
+    },
+    "monitor": {
+      defaultTitle: "Monitor",
+      defaultIcon: "monitor",
+      defaultDockId: "center",
+      component: MonitorWindow,
       parameters: {
         "inspector": true,
       }
